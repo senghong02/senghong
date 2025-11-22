a59785601b4ab4144c99f75a8181d668010f8742
SenghongH <senghong.hang@techbodia.com>
2025-11-22 22:22:09 +0700

    remove tag in add

--- app/app.vue
+++ app/app.vue
@@ -3,20 +3,4 @@
 </template>
 
 <script setup>
-useHead({
-  script: [
-    {
-      src: "https://www.googletagmanager.com/gtag/js?id=G-G5XX198FZB",
-      async: true,
-    },
-    {
-      children: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-G5XX198FZB');
-      `
-    }
-  ]
-})
 </script>
